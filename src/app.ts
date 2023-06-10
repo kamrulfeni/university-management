30cc78d349b66d421572d529d90959507e9935fa
kamrulHasan <kamrulhasandhaka704@gmail.com>
2023-06-10 18:02:00 +0600

    glovalErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,32 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response, NextFunction } from 'express'
 import cors from 'cors'
 //import userService from './app/modules/users/user.service'
 const app: Application = express()
 
 import usersRouter from './app/modules/users/user.route'
+import globalErrorHandler from './app/middlewares/globalErrorHandler'
+
 app.use(cors())
 // parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/users/', usersRouter)
+
+
 //testing
-// app.get('/',  (req: Request, res: Response) => {
-//   // await userService.createUser({
-//   //   id:'999',
-//   //   password:'1234',
-//   //   role:"student"
-//   // })
-//   res.send('Working successfully')
-//})
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//    throw new ApiError(400, 'Ore Baba Error')
+  //throw new Error('ore Baba Error')
+  // next('ore baba error')
+  //   // await userService.createUser({
+  //   //   id:'999',
+  //   //   password:'1234',
+  //   //   role:"student"
+  //   // })
+  //   res.send('Working successfully')
+  // global error handler
+// })
+app.use(globalErrorHandler)
 
 export default app
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, format, transports } from 'winston'
 const { combine, timestamp, label, printf, prettyPrint } = format
 import path from 'path'
+import DailyRotateFile from 'winston-daily-rotate-file'
 
 // custom log Format
 const myFormat = printf(({ level, message, label, timestamp }) => {
@@ -17,26 +18,50 @@ const logger = createLogger({
   format: combine(label({ label: 'ph' }), timestamp(), myFormat, prettyPrint()),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
-      level: 'info',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 
 const errorLogger = createLogger({
   level: 'error',
-  format: combine(
-    label({ label: 'right meow!' }),
-    timestamp(),
-    myFormat,
-  ),
+  format: combine(label({ label: 'ph!' }), timestamp(), myFormat),
   transports: [
     new transports.Console(),
-    new transports.File({
-      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
-      level: 'error',
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
     }),
   ],
 })
 export { logger, errorLogger }
+
+/**
+ filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'phu-%DATE%-error.log'
+      ),
+ */
