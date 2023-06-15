7fc41a692367afde2adbc2a75cda6afc37527c18
kamrulHasan <kamrulhasandhaka704@gmail.com>
2023-06-15 23:14:23 +0600

    try catch optimized

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,26 +1,42 @@
-import { RequestHandler } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
 import { AcademicSemesterService } from './academicSemester.service';
 
-
-const createSemester: RequestHandler = async (req, res, next) => {
-  try {
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const { ...academicSemesterData } = req.body;
-    const result = await AcademicSemesterService.createSemester(academicSemesterData);
-    res.status(200).json({
-      success: true,
-      message: 'Academic Semester created successfully!',
-      data: result,
-    });
-  } catch (err) {
-    next(err);
+    const result = await AcademicSemesterService.createSemester(
+      academicSemesterData
+    );
+    next();
+
+     sendResponse(res, {
+       statusCode: httpStatus.OK,
+       success: true,
+       message: 'Academic Semester created successfully!',
+       data: result,
+     });
+
+  }
+);
+
+export const AcademicSemesterController = {
+  createSemester,
+};
+
+
+
+    // res.status(200).json({
+    //   success: true,
+    //   message: 'Academic Semester created successfully!',
+    //   data: result,
+    // });
+
     // res.status(400).json({
     //   error: err,
 
     // success: false,
     // message: 'failed to create user',
     //})
-  }
-};
-export const AcademicSemesterController = {
-  createSemester,
-};
